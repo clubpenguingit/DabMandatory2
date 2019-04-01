bebb8019cc8c771c61ade4069051ab491c665a7a
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-04-01 11:38:50 +0200

    CourseConfiguration uploaded, not completed

---
+++ DABManatory2/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DABMandatory2.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DABMandatory2.EntityConfigurations
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+
+        }
+    }
+}
