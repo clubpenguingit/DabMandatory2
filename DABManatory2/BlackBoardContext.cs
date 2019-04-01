650363120584ed7cc39a784e645b90648d4a1b1c
k00ziex <andreas_vorgaard@hotmail.com>
2019-04-01 14:31:58 +0200

    DbSets created

--- DABManatory2/BlackBoardContext.cs
+++ DABManatory2/BlackBoardContext.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.EntityFrameworkCore;
-using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using DABMandatory2.Entities;
 
 
 namespace DABMandatory2
 {
     class BlackBoardContext : DbContext
     {
+        public DbSet<Assignments> Assignments { get; set; }
+        public DbSet<Calendar> Calendars { get; set; }
+        public DbSet<ContentArea> ContentAreas { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<CourseContent> CourseContents { get; set; }
+        public DbSet<CourseResponsible> CourseResponsibles { get; set; }
+        public DbSet<Deadlines> Deadlines { get; set; }
+        public DbSet<Folder> Folders { get; set; }
+        public DbSet<HandIns> HandIns { get; set; }
+        public DbSet<IsAssignedTo> IsAssignedTos { get; set; }
+        public DbSet<IsEnrolledTo> IsEnrolledTos { get; set; }
+        public DbSet<LectureDates> LectureDates { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<TeachingAssistant> TeachingAssistants { get; set; }
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "Data Source=DESKTOP-UGIDUH3;Initial Catalog=MandatoryDAB2;Integrated Security=True";
