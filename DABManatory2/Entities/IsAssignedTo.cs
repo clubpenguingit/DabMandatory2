5dcc08a5c4670d1925b80666eb3b9543e59c4c67
Simon Jensen <simonzhjensen@gmail.com>
2019-04-01 10:49:29 +0200

    Mooor, Jeg er færdig!

--- DABManatory2/Entities/IsAssignedTo.cs
+++ DABManatory2/Entities/IsAssignedTo.cs
@@ -1,7 +1,13 @@
-﻿namespace BlackBoard.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DABMandatory2.Entities
 {
     public class IsAssignedTo
     {
-        
+        [MaxLength(100)]
+        public string CourseID { get; set; }
+
+        [MaxLength(10)]
+        public string TeacherID { get; set; }
     }
 }
