9a48335a30cd469254ddd0541318d86c9ca2ac32
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-04-01 10:43:31 +0200

    Assignments upload

--- DABManatory2/BlackBoardContext.cs
+++ DABManatory2/BlackBoardContext.cs
@@ -11,10 +11,11 @@ namespace DABMandatory2
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connection = "String";
-
-            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(connection);
+            var connectionString = "Data Source=DESKTOP-UGIDUH3;Initial Catalog=MandatoryDAB2;Integrated Security=True";
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(connectionString);
+            
             //optionsBuilder.UseSqlServer(
             //    "Data Source=DESKTOP-UGIDUH3;Initial Catalog=MandatoryDAB2;Integrated Security=True");
+        }
     }
 }
--- DABManatory2/Entities/Assignments.cs
+++ DABManatory2/Entities/Assignments.cs
@@ -1,7 +1,38 @@
-﻿namespace BlackBoard.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BlackBoard.Entities;
+
+namespace DABMandatory2.Entities
 {
     public class Assignments
     {
-        
+        // Composite key not made here. 
+
+        [Required]
+        // TODO: Må kun være  -03, 00, 02, 04, 7, 10 & 12[Range()]
+        public int Grade { get; set; }
+
+        [Required]
+        public int  GroupSize { get; set; }
+
+        [Required]
+        public bool Passed { get; set; }
+
+        [MaxLength(255)]
+        public string Assignment_ID { get; set; }
+
+        [MaxLength(10)]
+        public string AU_ID { get; set; }
+
+        [MaxLength(100)]
+        public string Course_ID { get; set; }
+
+        [MaxLength(10)]
+        public string Teacher_ID { get; set; }
+
+        [Required]
+        public DateTime TIMESTAMP { get; set; }
+
+
     }
 }
