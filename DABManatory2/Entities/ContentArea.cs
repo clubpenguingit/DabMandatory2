849b89a1ee0fb06aeb72df47221780ea05c86750
ALE <holmern251@gmail.com>
2019-04-01 13:07:37 +0200

    FolderConfiguration + tilhørende entity ændringer

--- DABManatory2/Entities/ContentArea.cs
+++ DABManatory2/Entities/ContentArea.cs
@@ -1,7 +1,11 @@
-﻿namespace BlackBoard.Entities
+﻿using DABMandatory2.Entities;
+
+namespace BlackBoard.Entities
 {
     public class ContentArea
     {
+        public string Content_ID { get; set; }
+        public Folder Folder { get; set; }
         
     }
 }
--- DABManatory2/Entities/CourseContent.cs
+++ DABManatory2/Entities/CourseContent.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using DABMandatory2.Entities;
 using Microsoft.EntityFrameworkCore.Design;
 
 namespace BlackBoard.Entities
@@ -10,5 +12,7 @@ namespace BlackBoard.Entities
 
         [MaxLength(100)]
         public string Course_ID { get; set; }
+
+        public List<Folder> Folders { get; set; }
     }
 }
