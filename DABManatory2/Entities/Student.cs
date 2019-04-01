1358bbf5a7f4eea760b6a6ab705d0806878f162b
Simon Jensen <simonzhjensen@gmail.com>
2019-04-01 10:30:02 +0200

    VI PRØVER IGEN!

---
+++ DABManatory2/Entities/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlackBoard.Entities
+{
+    public class Student
+    {
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [MaxLength(10)]
+        public string AuID { get; set; }
+
+        [Required]
+        public DateTime GraduationDate { get; set; }
+
+        [Required]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
