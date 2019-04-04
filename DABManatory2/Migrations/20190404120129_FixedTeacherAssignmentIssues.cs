627cdda8ea255cb5511658b7dd45869977b2b97e
k00ziex <andreas_vorgaard@hotmail.com>
2019-04-04 14:01:55 +0200

    Fixed Teacher/Assignment issue

---
+++ DABManatory2/Migrations/20190404120129_FixedTeacherAssignmentIssues.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DABMandatory2.Migrations
+{
+    public partial class FixedTeacherAssignmentIssues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Assignments_Teacher_ID",
+                table: "Assignments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_Teacher_ID",
+                table: "Assignments",
+                column: "Teacher_ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Assignments_Teacher_ID",
+                table: "Assignments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_Teacher_ID",
+                table: "Assignments",
+                column: "Teacher_ID",
+                unique: true,
+                filter: "[Teacher_ID] IS NOT NULL");
+        }
+    }
+}
