5ffffd22e83d5a0d74d3f2666d750ccd2f377733
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-04-04 12:24:22 +0200

    Migration 2.6

--- DABManatory2/Migrations/20190404102117_OverhaulVersion2.6.Designer.cs
+++ DABManatory2/Migrations/20190404102117_OverhaulVersion2.6.Designer.cs
@@ -4,14 +4,16 @@ using DABMandatory2;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DABMandatory2.Migrations
 {
     [DbContext(typeof(BlackBoardContext))]
-    partial class BlackBoardContextModelSnapshot : ModelSnapshot
+    [Migration("20190404102117_OverhaulVersion2.6")]
+    partial class OverhaulVersion26
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
