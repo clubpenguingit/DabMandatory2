d667895f0d4a13e8745d88d820ce8822e71e33ce
Simon Jensen <simonzhjensen@gmail.com>
2019-04-04 13:11:46 +0200

    HJÆLP!

---
+++ DABManatory2/Repository/Implementation/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DABMandatory2.Entities;
+using DABMandatory2.Interfaces;
+
+namespace DABMandatory2.Repository.Implementation
+{
+    public class CourseRepository : Repository<Course>
+    {
+        public CourseRepository(BlackBoardContext blackBoardContext) : base(blackBoardContext) { }
+
+        public Tuple<IEnumerable<IsEnrolledTo>,IEnumerable<IsAssignedTo>> GetAssignees(Course entity)
+        {
+            return new Tuple<IEnumerable<Student>, IEnumerable<IsAssignedTo>>(
+                _context.Set<IsEnrolledTo>().Find(entity).Student,
+                _context.Set<IsAssignedTo>().Find(entity).Teacher);
+        }
+
+
+    }
+}
