2b5f51c82fc57573e1c384a110f651fe4b1074e0
Simon Jensen <simonzhjensen@gmail.com>
2019-04-04 10:26:41 +0200

    Generic IRepository added

---
+++ DABManatory2/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
+
+namespace DABMandatory2.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(params object[] keys);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(params object[] keys);
+    }
+}
